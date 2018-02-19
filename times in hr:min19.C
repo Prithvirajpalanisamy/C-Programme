5b719a35e314e53bca3348802040996f1891f2a3
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-19 16:32:45 +0530

    Create times in hr:min19.C

---
+++ times in hr:min19.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int t1,t2,n;
+printf("enter the time in hr:min");
+scanf("%d%d",&t1&t2);
+n=t1-t2;
+printf("the value of n is %d",n);
+getch();
+return 0;
+}
