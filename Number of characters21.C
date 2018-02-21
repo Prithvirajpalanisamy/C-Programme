c2287cf26dc6837d43e2e5c3af95c112868e283f
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-21 15:52:49 +0530

    Create Number of characters21.C

---
+++ Number of characters21.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100];
+printf("enter the character ");
+scanf("%d",&a);
+for(i=0;i<=a;i++)
+{
+if(num=1)
+{
+count++;
+}
+else
+{
+printf("print the number of letters ");
+}
+}
+getch();
+return 0;
+}
