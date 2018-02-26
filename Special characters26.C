4e1c61e26d2e4db1dfcb9fdc8a5d4e5877195d20
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-26 16:38:46 +0530

    Create Special characters26.C

---
+++ Special characters26.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+int c = 0;
+char str[1000];
+fgets(str,1000,stdin);
+for(int i = 0; str[i]!='\0'; i++)
+{
+if(!isalpha(str[i]) && !isdigit(str[i]))
+c++;
+}
+printf("%d",c);
+getch();
+return 0;
+}
