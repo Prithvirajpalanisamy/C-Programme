f3ec0c3e54b703262c450a68ea4ab22694c8b23c
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-02 13:45:22 +0530

    Create Two strings.C

---
+++ Two strings.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int a,b;
+char x[10],y[10];
+printf("enter the string ");
+scanf("%s",&x);
+printf("enter the string ");
+scanf("%s",&y);
+a=strlen(x);
+b=strlen(y);
+if(a>=b)
+{
+printf("%s",x);
+}
+else
+{
+printf("%s",y);
+getch();
+return 0;
+}
