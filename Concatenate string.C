404e562905c4bc6144ad6281fb416d49baa3c376
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-05 15:43:34 +0530

    Create Concatenate string.C

---
+++ Concatenate string.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[20],s1[70];
+int i=0,j=o;
+printf("enter first string");
+gets(s);
+printf("enter second string");
+gets(s1);
+while(s[i]!='\0')
+i++;
+while(s1[j]!='\0')
+{
+s=s1;
+j++;
+i++;
+}
+s='\0';
+printf("%s",&s);
+getch();
+return 0;
+}
