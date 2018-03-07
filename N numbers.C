2b855dd8eca86dcdeceaef888b5c5ca7a9f31809
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-07 15:49:13 +0530

    Create N numbers.C

---
+++ N numbers.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10];
+int i,avg;
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<5;i++)
+{
+sum+=a[i];
+}
+avg=sum/5;
+printf("%d",avg);
+}
