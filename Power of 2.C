eb2cefe8c0f61b0c5539b3322475d415c3ce1e67
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-07 16:10:13 +0530

    Create new file

---
+++ Power of 2.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,a;
+scanf("%d",&n);
+for(i=1;i<=a;i++)
+{
+if(pow(2,i)==a)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
+}
