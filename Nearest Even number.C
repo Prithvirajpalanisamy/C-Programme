62d8120292520adb38e9fc64a34f99916dd4941e
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-14 16:21:46 +0530

    Create Nearest Even number.C

---
+++ Nearest Even number.C
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+{
+ int n,i;
+printf("enter the number:");
+    scanf("%d",&n);
+    if(n%2!=0)
+    {
+        printf("%d",n);
+    }
+    else
+    {
+        n--;
+        printf("%d",n);
+    }
+    return 0;
+}
+ 
