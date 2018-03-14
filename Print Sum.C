29b79b46e744d3d509bffb0af72b0d1ed2422280
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-14 15:58:41 +0530

    Create Print Sum.C

---
+++ Print Sum.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0,a[10];
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the integer");
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+scanf("%d",&a[i]);
+printf("sum of numbers = %d",sum);
+getch();
+return 0;
+}
