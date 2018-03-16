91238825d82bfea60388dfda610dbfc7c447b019
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-16 15:57:59 +0530

    Rename Swap 2 numbers.C to Swap numbers.C

---
+++ Swap numbers.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,t;
+printf("ENTER THE INPUT NOS\n");
+scanf("%d%d",&a,&b);
+printf("THE NOS BEFORE SWAPPING:\n%d\n%d\n",a,b);
+t=a;
+a=b;
+b=t;
+printf("\nTHE NOS AFTER SWAPPING:\n%d\n%d\n",a,b);
+getch();
+return 0;
+}
