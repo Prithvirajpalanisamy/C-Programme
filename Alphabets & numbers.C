810d74f9d7139dc132fff2f0b72051676d3012f6
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-16 15:51:01 +0530

    Create Alphabets & numbers.C

---
+++ Alphabets & numbers.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+char a[1000]="hello123";
+int i,b=0;
+for(i=0;i<1000;i++)
+{
+if((a[i]>='A'||a[i]<='Z'||a[i]>='a'||a[i]<='z')&&(a[i]>='0'||a[i]<='9'))
+{
+b++;
+}
+}
+if(b>0)
+{
+printf("yes");
+}
+return 0;
+}
