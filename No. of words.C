3337d3d1b71fc04d2b8d5711a594e4c71e2e4836
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-19 16:15:04 +0530

    Create No. of words.C

---
+++ No. of words.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,c=0;
+char a[50]="laptop is good";
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+c++;
+}
+}
+printf("number of words %d",c+1);
+getch();
+return 0;
+}
