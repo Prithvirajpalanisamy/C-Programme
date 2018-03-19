a26212fa75dbea30af73e342b8ce313cc42d8798
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-19 15:48:03 +0530

    Create Num&Char.C

---
+++ Num&Char.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int k,n,i,rem;
+char ch[100];
+printf("enter the string");
+scanf("%s",&ch);
+printf("enter the number");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+n=ch%10;
+ch=ch/10;
+rem=rem+ch;
+printf("%d",rem);
+}
+getch();
+return 0;
+}
