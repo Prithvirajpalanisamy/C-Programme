07679665c0455d28bd845ced5fa71fea4bcd13bd
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-23 03:23:35 -0700

    Create 2 numbers difference.C

---
+++ 2 numbers difference.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n enter the numbers");
+scanf("%d%d",&a,&b);
+c=a-b;
+if(c/2==0)
+{
+printf("\n even");
+}
+else
+{
+printf("\n odd");
+}
+}
