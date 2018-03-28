11483a550f5f9a0ffc80a359af3b731213ab51f2
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-28 15:58:16 +0530

    Create Perfect square.C

---
+++ Perfect square.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+int m,v;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+ for(m=0;m<=n;m++)
+ {
+   if(v=m*m)
+{
+printf("its a perfect square");
+}
+else 
+{
+printf("its is not a prefect square");
+}
+}
+getch();
+return 0;
+}
