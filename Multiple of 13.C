053918cf4d7063447659b526481e5e00c0f1a2bc
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-28 15:54:32 +0530

    Create Multiple of 13.C

---
+++ Multiple of 13.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter  a numbre");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+return 0;
+}
