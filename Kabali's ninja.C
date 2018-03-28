0adfe3296003ee918835a5446d1fd95f79d80064
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-03-28 16:15:06 +0530

    Create Kabali's ninja.C

---
+++ Kabali's ninja.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x[10],y[10],diff[10],i;
+printf("enter number of ninja's in kabali team and opponent team\n");
+for(i=0;i<10;i++)
+{
+scanf("%d%d",&x[i],&y[i]);
+}
+for(i=0;i<10;i++)
+{
+diff[i]=y[i]-x[i];
+printf("%d\n",diff[i]);
+}
+getch();
+return 0;
+}
